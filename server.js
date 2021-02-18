b4fa051b8ee222eaf0d75be6a03238a04d153ab8
EgiVilza <egivilza@gmail.com>
2021-02-17 19:58:26 -0600

    Create Folders and initial code files

---
+++ server.js
@@ -0,0 +1,25 @@
+const express = require('express');
+
+const indexRouter = require('./routes/html-routes.js');
+
+// Sets up the Express App
+const app = express();
+const PORT = process.env.PORT || 8080;
+
+// Requiring our models for syncing
+const db = require('./models');
+
+// Sets up the Express app to handle data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// Static directory
+app.use(express.static('public'));
+
+// Invoke routes
+indexRouter(app);
+
+// Syncing our sequelize models and then starting our Express app
+db.sequelize.sync({ force: true }).then(() => {
+  app.listen(PORT, () => console.log(`Listening on PORT ${PORT}`));
+});
