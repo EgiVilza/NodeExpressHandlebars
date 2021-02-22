9e5b350ff4ae444281e60d17293f90593cc09cec
EgiVilza <egivilza@gmail.com>
2021-02-21 19:53:57 -0600

    Finalized comments

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -3,10 +3,17 @@ const db = require("../models")
 
 // Routes
 module.exports = (app) => {
+
+    app.get("/", function(req, res) {
+        res.json(path.join(__dirname, "public/index.html"));
+      });
+      
+    //Finds all non-devoured burgers
     app.get('/api/burgers', (req,res) => {
         db.burgers.findAll({where: {devourStatus: false}}).then((results) => res.json(results))
     })
 
+    //Post burger record
     app.post('/api/burgers', (req, res) => {
         db.burgers.create({
             name: req.body.name,
@@ -14,6 +21,7 @@ module.exports = (app) => {
         }).then((results) = res.json(results))
     })
 
+    //Update burger record
     app.put('/api/burgers', (req, res) => {
         db.burgers.update(
             {
